60dee2f6446dca6e386e6d76b4ada2669bd3c5a8
soizi33 <fabian.salzlechner@gmail.com>
2025-10-26 18:56:53 +0100

    Create main.js

---
+++ main.js
@@ -0,0 +1,8 @@
+export function onStart() {
+  console.log("Windows Accent Color Sync Add-on gestartet!");
+  // Hier könntest du später deine Farb-Logik einbauen
+}
+
+export function onStop() {
+  console.log("Windows Accent Color Sync Add-on gestoppt.");
+}
